6b243e78af200d9310e04aac5a10aed5bd51adeb
fidannh <fidannh@code.edu.az>
2024-02-27 22:16:27 +0400

    Completed

---
+++ BrandTask1/DAL/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BrandTask1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrandTask1.DAL
+{
+    public class AppDbContext:DbContext
+    {
+
+        public DbSet<Brand> Brands { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSqlLocalDb;Database=BrandsDb;Trusted_Connection=true");
+        }
+    }
+}
---
+++ BrandTask1/Program.cs
@@ -0,0 +1,41 @@
+﻿using BrandTask1.DAL;
+using BrandTask1.Models;
+
+
+foreach (var item in GetAllBrands())
+{
+    Console.WriteLine(item);
+}
+
+void AddBrand()
+{
+   AppDbContext dbcontext = new AppDbContext();
+    var newBrand = new Brand
+    {
+        Name = "Bmw"
+    };
+    dbcontext.Brands.Add(newBrand);
+    dbcontext.SaveChanges();
+}
+
+List<Brand> GetAllBrands()
+{
+    AppDbContext dbcontext = new AppDbContext();
+    return dbcontext.Brands.ToList();
+}
+
+void UpdateBrandName()
+{
+    AppDbContext dbcontext = new AppDbContext();
+    var brand = dbcontext.Brands.First();
+    brand.Name = "Porsche";
+    dbcontext.SaveChanges();
+}
+
+void DeleteBrand()
+{
+    AppDbContext dbcontext = new AppDbContext();
+    var brand = dbcontext.Brands.First();
+    dbcontext.Brands.Remove(brand);
+    dbcontext.SaveChanges();
+}
